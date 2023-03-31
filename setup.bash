9c240652f327237c4b7bf1ce5d24cdcf2f6cdc4e
Shine Zhang <zhxin@vmware.com>
2023-03-31 15:45:08 -0500

    Add ffmpeg
    Also regroup the packages

--- setup.bash
+++ setup.bash
@@ -6,16 +6,14 @@ set -x
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew necessary packages
 brew install rectangle # managing windows
 brew install iterm2 # much better terminal
-
 brew install menumeters # good indicator of resource utilizations
 brew install pingmenu # good indicator of network ping
 
 brew install visual-studio-code # Microsoft IDE
 brew install microsoft-edge # Microsoft browser
-
 brew install --cask docker # Docker Desktop
-
 brew install tmux
+
+brew install ffmpeg
